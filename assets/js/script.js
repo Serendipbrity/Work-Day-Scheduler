b0ae75dc17bc3d210da0e67e88951d52db543d01
Brittani <96275616+Serendipbrity@users.noreply.github.com>
2022-04-05 12:13:10 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,19 @@
 
 
-// var today = new Date();
-
-// var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 var current = document.querySelector("#currentDay");
 
 let date = new Date()
 let day = date.getDate();
-let month = date.getMonth()+1;
+let month = date.getMonth()  + 1;
 let year = date.getFullYear();
 let currentHour = date.getHours();
 console.log(currentHour);
 
 let fullDate = `${month}.${day}.${year}`;
-console.log(fullDate);
+console.log("full date: " + fullDate);
 
 
 current.innerHTML = "Date: " + fullDate;
-
-
 
 var allTimes = document.querySelector(".container");
 allTimes.addEventListener("click", function(event) {
@@ -38,10 +33,19 @@ allTimes.addEventListener("click", function(event) {
    localStorage.setItem(timeId, textBox);
 }  
 )
+// var nine = 9;
+// var ten = 10;
+// var eleven = 11;
+// var twelve = 12;
+// var one = 13;
+// var two = 14;
+// var three = 15;
+// var four = 16;
+// var five = 17;
 
-var hours = [nine,ten,eleven,twelve,one,two,three,four,five];
 
-   var nine = document.querySelector('#nine .description').value = localStorage.getItem('nine');
+
+  var nine = document.querySelector('#nine .description').value = localStorage.getItem('nine');
    var ten = document.querySelector('#ten .description').value = localStorage.getItem('ten');
    var eleven = document.querySelector('#eleven .description').value = localStorage.getItem('eleven');
    var twelve =document.querySelector('#twelve .description').value = localStorage.getItem('twelve');
@@ -51,24 +55,18 @@ var hours = [nine,ten,eleven,twelve,one,two,three,four,five];
    var four = document.querySelector('#four .description').value = localStorage.getItem('four');
    var five =document.querySelector('#five .description').value = localStorage.getItem('five');
 
-nine = 9;
-ten = 10;
-eleven = 11;
-twelve = 12;
-one = 13;
-two = 14;
-three = 15;
-four = 16;
-five = 17;
+   var hours = [nine,ten,eleven,twelve,one,two,three,four,five];
+   console.log(hours);
 
+//    9 = document.getElementById("nine");
 
 function getHour(){
     for(i = 0; i < hours.length; i++){
  if (currentHour = hours[i])
-        hours[i].add.className("present");
+        hours[i].classList.add("present");
  if (currentHour < hours[i])
-        hours[i].add.classList("future");
+        hours[i].classList.add("future");
  if (currentHour > hours[i])
-        hours[i].add.classList("past");     
+        hours[i].classList.add("past");     
 }
 };
